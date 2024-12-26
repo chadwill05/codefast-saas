78899d7ec545ad86d86664d43c9bb362057e4928
Chad Williams <chadwill05@gmail.com>
2024-12-26 09:11:12 -0500

    edit test

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Collect customer feedback to build better products</h1>
+      <h2>Hey</h2>
       <div>
         Create a feedback board in minutes, priotize features, and 
         build products your customers will love.
