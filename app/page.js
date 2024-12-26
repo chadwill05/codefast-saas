fd5b46dfe788c017eea3a31c08321ce3aaa215c9
Chad Williams <chadwill05@gmail.com>
2024-12-26 17:56:30 -0500

    saving what I did so far

--- app/page.js
+++ app/page.js
@@ -7,13 +7,30 @@ function Home() {
 
   return (
     <main>
-      <h1 className="title extra-space">Collect customer feedback to build better products</h1>
-      <div>
+      <section className="bg-base-200">
+        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+         <div className="font-bold">CodeFastSaaS</div>
+         <div className="space-x-4 max-md:hidden">
+          <a className="link link-hover">Pricing</a>
+          <a className="link link-hover">FAQ</a>
+         </div>
+        <div>
+      </div>
+          <ButtonLogin isLoggedIn={isLoggedIn} name={name}/>
+        </div>
+   
+
+
+      </section>
+      <section className="px-8 text-center py-32 max-w-3xl mx-auto">
+      <h1 className="text-4xl font-extrabold mb-6">
+        Collect customer feedback to build better products</h1>
+      <div className="opacity-90 mb-10">
         Create a feedback board in minutes, priotize features, and 
         build products your customers will love.
       </div>
       <ButtonLogin isLoggedIn={isLoggedIn} name={name}/>
-        
+      </section>
     </main>
   );
 }
--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 // This button is used to log users in or send them to the dashboard
 const ButtonLogin = (isLoggedIn, name) => {
     if (isLoggedIn) {
-        return <Link href="/dashboard">Welcome back {name}</Link>
+        return <Link href="/dashboard" className="btn btn-primary">
+            Welcome back {name}</Link>
     } 
     return <button>Login</button>;
 };
