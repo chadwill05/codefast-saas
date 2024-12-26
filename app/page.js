2309479a9ab10e7c2d094138862bfe56bde38dfc
Chad Williams <chadwill05@gmail.com>
2024-12-26 09:25:39 -0500

    added dashboard with link

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         Create a feedback board in minutes, priotize features, and 
         build products your customers will love.
       </div>
+      <Link href="/dashboard">Dashboard</Link>
     </main>
   );
 }
