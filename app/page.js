86daa9742107dcaf92000a9c2b240c374c450847
Chad Williams <chadwill05@gmail.com>
2024-12-26 12:24:08 -0500

    commit changes

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 
 import ButtonLogin from "@/components/ButtonLogin";
 
-export default function Home() {
+function Home() {
   const isLoggedIn = true;
   const name = "Chad";
 
   return (
     <main>
-      <h1>Collect customer feedback to build better products</h1>
-      <h2>Hey</h2>
+      <h1 className="title extra-space">Collect customer feedback to build better products</h1>
       <div>
         Create a feedback board in minutes, priotize features, and 
         build products your customers will love.
@@ -18,3 +17,6 @@ export default function Home() {
     </main>
   );
 }
+
+export default Home;
+
